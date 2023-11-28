a721496dfbf387ebdf6bae4f0da7e8098e3b4397
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-11-28 21:22:51 +0200

    home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const Homes = () => {
+  const { user } = useSelector(state => state.user)
+  return (
+    <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden'>
+      home
+    </div>
+  )
+}
+
+export default Homes
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-import Home from "./Homes";
+import Home from "./Home";
 import Login from "./Login";
 import Profile from "./Profile";
 import Register from "./Register";
