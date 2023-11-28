9248c86f6049a68476aecb84e4d7660a6a8c860d
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-11-28 18:54:22 +0200

    login page

--- client/src/redux/theme.js
+++ client/src/redux/theme.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    theme: JSON.parse(window?.localStorage.getItem("theme")) ?? "dark"
+    theme: JSON.parse(window?.localStorage.getItem("theme")) ?? "light"
 }
 
 console.log(initialState)
