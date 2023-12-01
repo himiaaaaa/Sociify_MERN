805863c9715b24f80e998406cc5496b05e3d4f67
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-01 17:16:04 +0200

    mongoDB models

---
+++ server/models/passwordReset.js
@@ -0,0 +1,19 @@
+import Mongoose, { Schema }  from "mongoose";
+
+const passwpordResetSchema = Schema({
+    userId: {
+        type: String,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    token: String,
+    createdAt: Date,
+    expiresAt: Date
+})
+
+const PasswordReset = mongoose.model("PasswordReset", passwpordResetSchema)
+
+export default PasswordReset;
---
+++ server/models/userModel.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [ true, "First name is required!" ]
+    },
+    lastName: {
+        type: String,
+        required: [ true, "Last name is required!" ]
+    },
+    email: {
+        type: String,
+        requirqed: [true, "Email is required!"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required!"],
+        minlength: [6, "Password length should be greater than 6 character"],
+        select: true
+    },
+    localtion: {
+        type: String
+    },
+    profileUrl: {
+        type: String
+    },
+    profession: {
+        type: String
+    },
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: "Users"
+    }],
+    views:[{
+        type: String
+    }],
+    verified: {
+        type: Boolean,
+        default: false
+    }
+},
+{
+    timestamps: true
+})
+
+const Users = mongoose.model("Users", userSchema)
+
+export default Users;
