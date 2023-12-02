aef6beb79b1a5d155acbf8e1f0783164e8718b5c
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-02 20:20:06 +0200

    password reset done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,25 @@
 import express from "express";
 import path from "path";
-import { verifyEmail } from "../controllers/userController.js";
+import { requestPasswordReset, verifyEmail, resetPassword, changePassword } from "../controllers/userController.js";
+
 
 const router = express.Router();
 const __dirname = path.resolve(path.dirname(""))
 
 router.get("/verify/:userId/:token", verifyEmail)
 
+//Password reset 
+router.post("/request-passwordreset", requestPasswordReset) //User asks to reset password, gets a reset link.
+router.get("/reset-password/:userId/:token", resetPassword) //User clicks link, sees form, sets new password.
+router.post("/reset-password", changePassword) //User submits form, password is updated.
+
 router.get("/verified", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/build", "index.html"))
 })
+
+router.get("/resetpassword", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/build", "index.html"))
+})
+
 
 export default router;
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import { v4 as uuidv4 } from "uuid"
 import { hashString } from "./index.js"
 import Verification from "../models/emailVerification.js"
+import PasswordReset from "../models/passwordReset.js"
 
 dotenv.config()
 
@@ -83,3 +84,55 @@ export const sendVerificationEmail = async(user, res) => {
     }
 }
 
+export const resetPasswordLink = async(user, res) => {
+    const { _id, email } = user
+
+    const token = _id + uuidv4()
+
+    const link = APP_URL + "users/reset-password/" + _id + "/" + token;
+
+    const mailOptions = {
+        from: AUTH_EMAIL,
+        to: email,
+        subject: "Password Reset",
+        html: `
+            <p style="font-family: Arial, sans-serif; font-size: 16px; color: #333; background-color: #f7f7f7; padding: 20px; border-radius: 5px;">
+                Password reset link. Please click the link below to reset password.
+                <br>
+                <p style="font-size: 18px;"><b>This link expires in 10 minutes</b></p>
+                <br>
+                <a href=${link} style="color: #fff; padding: 10px; text-decoration: none; background-color: #000;  border-radius: 8px; font-size: 18px;">Reset Password</a>.
+            </p>`
+    }
+
+    try {
+        const hashedToken = await hashString(token)
+
+        const resetEmail = await PasswordReset.create({
+            userId: _id,
+            email:email,
+            token: hashedToken,
+            createdAt: Date.now(),
+            expiresAt: Date.now() + 600000
+        })
+
+        if(resetEmail){
+            transporter
+                .sendMail(mailOptions)
+                .then(() => {
+                    res.status(201).send({
+                        success: "Pending",
+                        message: "Reset Password Link has been sent to your account."
+                    })
+                })
+                .catch((error) => {
+                   console.log(error)
+                   res.status(404).json({ message: "Something went wrong" })
+                })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(404).json({ message: "Something went wrong" })
+    }
+}
