287e8461c395cbb00b27ffb508678ba36ee55417
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-04 10:33:51 +0200

    get posts

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,10 +1,15 @@
 import express, { Router } from "express"
 import userAuth from "../middleware/authMiddleware.js"
-import { createPost } from "../controllers/postController.js"
+import { createPost, getPosts, getPost } from "../controllers/postController.js"
 
 const router = express.Router()
 
 //create post
 router.post("/create-post", userAuth, createPost)
+
+//get post
+router.post("/", userAuth, getPosts)
+
+
 
 export default router
