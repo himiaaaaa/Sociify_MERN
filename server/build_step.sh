1179984c593a65f586a3e56c6b0e847499c12ab5
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-01-10 18:58:41 +0200

    update

--- server/build_step.sh
+++ server/build_step.sh
@@ -4,5 +4,5 @@ echo "Build script"
 
 # add the commands here
 
-npm install && cd ../client && npm install && cd ..
+npm install && cd ../client && npm install && cd ../server
 rm -rf build && cd ../client && npm run build && cp -r build ../server
